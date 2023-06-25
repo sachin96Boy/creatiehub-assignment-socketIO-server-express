bba80b7868222298149a5d6b0765627acefaebf7
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-06-25 19:44:22 +0530

    chor: mouse emit function added

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
-const cors = require('cors');
-const app = require('express')();   /* this will import and invoke the function at the same time */
+const cors = require("cors");
+const app =
+  require("express")(); /* this will import and invoke the function at the same time */
 app.use(cors());
-const server = require('http').createServer(app);
-const io = require('socket.io')(server,{
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
   cors: {
-    origin: '*',
-  }
+    origin: "*",
+  },
 });
 const port = process.env.PORT || 8080;
 
+var listOfUsers = [];
 
-io.on('connection', (socket) => {
-  console.log('user connected');
-
-  socket.on('emituserdata',(dataFromClient)=>{
-    console.log(dataFromClient);
-  })
-
-  socket.on('disconnect', function () {
-    console.log('user disconnected');
+io.on("connection", (socket) => {
+  console.log("user connected");
+  listOfUsers.push(socket.id);
+  socket.on("emituserdata", (dataFromClient) => {
+    listOfUsers.forEach((userId) => {
+      if (userId != socket.id) {
+        socket.emit("sendDatatoClient", dataFromClient);
+      }
+    });
   });
-})
-server.listen(port, function() {
+
+  socket.on("disconnect", function () {
+    console.log("user disconnected");
+  });
+});
+server.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
