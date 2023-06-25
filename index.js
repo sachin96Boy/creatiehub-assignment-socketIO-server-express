7fcd02f8f647f4b85dae85328547c50637447e9b
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-06-25 12:58:34 +0530

    chor: cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+const cors = require('cors');
 const app = require('express')();   /* this will import and invoke the function at the same time */
+app.use(cors());
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const port = process.env.PORT || 8080;
+
 
 io.on('connection', (socket) => {
   console.log('user connected');
