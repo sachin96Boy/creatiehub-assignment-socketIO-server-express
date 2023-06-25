0552294163f09f35ef185a183e1ec3647b0eda54
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-06-25 11:12:41 +0530

    init: socketIO server with express

---
+++ index.js
@@ -0,0 +1,14 @@
+const app = require('express')();   /* this will import and invoke the function at the same time */
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const port = process.env.PORT || 8080;
+
+io.on('connection', (socket) => {
+  console.log('user connected');
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+  });
+})
+server.listen(port, function() {
+  console.log(`Listening on port ${port}`);
+});
